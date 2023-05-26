d8f7226372a2a2b49e37c122c1543025489defec
Darius Tanz <scratchticketseb@gmail.com>
2023-05-26 13:11:35 +0000

    May 26, 2023, 9:11 AM

--- CoinSort/src/main.js
+++ CoinSort/src/main.js
@@ -1,21 +1,19 @@
 let Coins = document.getElementById('Coins');
 
 function AddCoin(name, ticker, marketCap, price) {
-    // Clone the Bitcoin container
     const btcContainer = document.getElementById("BTC");
     const newContainer = btcContainer.cloneNode(true);
   
-    // Update the content of the cloned container
-    newContainer.querySelector(".logo img").alt = name + " logo";
-    newContainer.querySelector(".info span:first-child").textContent = name;
-    newContainer.querySelector(".info .ticker").textContent = "Ticker: " + ticker;
-    newContainer.querySelector(".info .market-cap").textContent = "Market Cap: " + formatMarketCap(marketCap);
-    newContainer.querySelector(".info .price").textContent = "Price: " + price;
+    newContainer.id = ticker;
+    newContainer.querySelector(".logo img").src = ""; // Set the logo URL here
+    newContainer.querySelector(".info #BTC-name").textContent = name;
+    newContainer.querySelector(".info .ticker #BTC-ticker").textContent = ticker;
+    newContainer.querySelector(".info .market-cap #BTC-market-cap").textContent = formatMarketCap(marketCap);
+    newContainer.querySelector(".info .price #BTC-price").textContent = price;
   
-    // Append the cloned container to the Coins div
-    const coinsDiv = document.getElementById("Coins");
-    coinsDiv.appendChild(newContainer);
+    btcContainer.parentNode.appendChild(newContainer);
   }
+  
   
   // Format the market cap value
   function formatMarketCap(marketCap) {
