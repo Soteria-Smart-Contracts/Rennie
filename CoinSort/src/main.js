1fcaee812c12e87ecbd607b7d8d29caa5aa9aa78
Darius Tanz <scratchticketseb@gmail.com>
2023-05-26 13:00:57 +0000

    May 26, 2023, 9:00 AM

--- CoinSort/src/main.js
+++ CoinSort/src/main.js
@@ -1,6 +1,34 @@
 let Coins = document.getElementById('Coins');
 
-
+function addCoinContainer(name, ticker, marketCap, price) {
+    // Clone the Bitcoin container
+    const btcContainer = document.getElementById("BTC");
+    const newContainer = btcContainer.cloneNode(true);
+  
+    // Update the content of the cloned container
+    newContainer.querySelector(".logo img").alt = name + " logo";
+    newContainer.querySelector(".info span:first-child").textContent = name;
+    newContainer.querySelector(".info .ticker").textContent = "Ticker: " + ticker;
+    newContainer.querySelector(".info .market-cap").textContent = "Market Cap: " + formatMarketCap(marketCap);
+    newContainer.querySelector(".info .price").textContent = "Price: " + price;
+  
+    // Append the cloned container to the Coins div
+    const coinsDiv = document.getElementById("Coins");
+    coinsDiv.appendChild(newContainer);
+  }
+  
+  // Format the market cap value
+  function formatMarketCap(marketCap) {
+    const num = parseInt(marketCap.replace(/\D/g, ""));
+    if (num >= 1000000000) {
+      return (num / 1000000000).toFixed(2) + "B";
+    } else if (num >= 1000000) {
+      return (num / 1000000).toFixed(2) + "M";
+    } else {
+      return marketCap;
+    }
+  }
+  
 
 function ShowCoins(Setting){
     if(Setting){
